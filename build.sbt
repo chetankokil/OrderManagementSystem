ece9d19a575158a588bf1b67d4952ef101c9dde5
chetankokil <chetankokil@gmail.com>
2021-05-03 21:01:16 -0500

    Initial checkin

---
+++ build.sbt
@@ -0,0 +1,15 @@
+name := "OrderManagement"
+
+version := "0.1"
+
+scalaVersion := "2.12.13"
+
+val AkkaVersion = "2.6.8"
+val AkkaHttpVersion = "10.2.4"
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
+  "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+  "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
+  "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion
+
+)
---
+++ src/main/scala/OMSServer.scala
@@ -0,0 +1,83 @@
+import akka.Done
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import spray.json.DefaultJsonProtocol._
+
+import scala.concurrent.Future
+import scala.io.StdIn
+
+object OMSServer {
+
+  implicit val system = ActorSystem(Behaviors.empty, "System")
+  implicit val ec = system.executionContext
+  var orders: List[Order] = Nil
+
+
+  final case class Item(name: String, id: Long)
+  final case class Order( items: List[Item], id:Long)
+
+
+  implicit val itemFormat = jsonFormat2(Item)
+  implicit val orderFormat = jsonFormat2(Order)
+
+  // (fake) async database query api
+  def fetchItem(itemId: Long): Future[Option[Item]] = Future {
+    orders.flatMap(o => o.items).find(item => item.id == itemId)
+  }
+
+  def fetchOrder(orderId: Long): Future[Option[Order]] = Future {
+    orders.foreach(println)
+    orders.find(o => o.id == orderId)
+  }
+
+  def saveOrder(order: Order) : Future[Done] = {
+    orders = order match {
+      case Order(id, items) => Order(id,items) :: orders
+      case _ => orders
+    }
+
+    Future { Done }
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    val route =
+      concat(
+        get {
+          pathPrefix("orders"/ LongNumber) { id =>
+            val mayBeOrder = fetchOrder(id)
+            onSuccess(mayBeOrder) {
+              case Some(order)  => complete(order)
+              case None         => complete(StatusCodes.NotFound)
+            }
+          }
+        },
+
+        post {
+          path("orders") {
+            entity(as[Order]) { order =>
+              val saved = saveOrder(order)
+              onSuccess(saved) {
+                _ => complete("order created")
+              }
+            }
+          }
+        }
+      )
+
+
+
+
+    val bindingFuture = Http().newServerAt("localhost",8080).bind(route)
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+
+  }
+}
